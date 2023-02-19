5555285dd7a08f8c53e245606868e2f3a4b1177d
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-02-19 11:32:38 +0530

    Update app.js

--- FrontEnd/server/app.js
+++ FrontEnd/server/app.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 //password =5hqiaFjBsOSpjhyc
+const mongoUri =
+  "mongodb+srv://Ravihara:5hqiaFjBsOSpjhyc@cluster0.iaxel6r.mongodb.net/?retryWrites=true&w=majority";
+
 app.get("/", (req, res) => {
   res.send("welcome to node js");
 });
