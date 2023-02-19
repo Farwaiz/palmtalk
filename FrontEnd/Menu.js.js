368d9f5ca3b143bdc068d29f149075bb9b58f178
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-02-19 18:51:48 +0530

    Update Menu.js.js

--- FrontEnd/Menu.js.js
+++ FrontEnd/Menu.js.js
@@ -1,24 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, TouchableOpacity} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   logo: {
-    resizeMode: 'center',
-    position: 'absolute',
+    resizeMode: "center",
+    position: "absolute",
     top: 0,
   },
-  buttons:{ 
+  buttons: {
     borderRadius: 8,
     marginBottom: 20,
     paddingHorizontal: 10,
     paddingVertical: 14,
-    backgroundColor: '#fabd02',
+    backgroundColor: "#fabd02",
     bottom: 150,
     marginLeft: 30,
     marginRight: 30,
@@ -29,55 +36,53 @@ const styles = StyleSheet.create({
   },
 
   buttontext: {
-    color: 'white',
+    color: "white",
     fontSize: 8,
-    textAlign: 'center',
-    
+    textAlign: "center",
   },
 });
 
 const Display = () => {
   return (
-    <><View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={require('../AwesomeProject/assets/PalmTalk.png')} />
-    </View>
-
-    <TouchableOpacity>
-      <View style={styles.buttons}>
-        <Text style={styles.buttontext}>Sign Language To Sinhala Text</Text>
+    <>
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require("../AwesomeProject/assets/PalmTalk.png")}
+        />
       </View>
-    </TouchableOpacity>
 
-    <View style={styles.space} />
+      <TouchableOpacity>
+        <View style={styles.buttons}>
+          <Text style={styles.buttontext}>Sign Language To Sinhala Text</Text>
+        </View>
+      </TouchableOpacity>
 
-    <TouchableOpacity>
-      <View style={styles.buttons}>
-        <Text style={styles.buttontext}>History</Text> 
-      </View>
-    </TouchableOpacity>
+      <View style={styles.space} />
 
-    <View style={styles.space} />
+      <TouchableOpacity>
+        <View style={styles.buttons}>
+          <Text style={styles.buttontext}>History</Text>
+        </View>
+      </TouchableOpacity>
 
-    <TouchableOpacity>
-      <View style={styles.buttons}>
-        <Text style={styles.buttontext}>Setting</Text> 
-      </View>
-    </TouchableOpacity>
+      <View style={styles.space} />
 
-    <View style={styles.space} />
+      <TouchableOpacity>
+        <View style={styles.buttons}>
+          <Text style={styles.buttontext}>Setting</Text>
+        </View>
+      </TouchableOpacity>
 
-    <TouchableOpacity>
-      <View style={styles.buttons}>
-        <Text style={styles.buttontext}>About Us</Text> 
-      </View>
-    </TouchableOpacity></>
+      <View style={styles.space} />
 
+      <TouchableOpacity>
+        <View style={styles.buttons}>
+          <Text style={styles.buttontext}>About Us</Text>
+        </View>
+      </TouchableOpacity>
+    </>
   );
-}
+};
 
 export default Display;
-
-
-
