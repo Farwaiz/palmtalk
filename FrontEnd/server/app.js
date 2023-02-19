75df48dc82b3073d4b1bd0ca98b870010f986125
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-02-19 11:27:18 +0530

    Update app.js

--- FrontEnd/server/app.js
+++ FrontEnd/server/app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+//password =5hqiaFjBsOSpjhyc
 app.get("/", (req, res) => {
   res.send("welcome to node js");
 });
