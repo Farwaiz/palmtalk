be4d523fd5ef2eba6ed58283d1a2c31968809d73
yusuf khaleel <ahamedyusufk@gmail.com>
2023-02-19 22:51:32 +0530

    Revert "Update App.js"
    This reverts commit 31b8c1f978016114e94a1b380bd15dac91e8cdfc.

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,102 +1,93 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image, Text, TextInput } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View,Image ,Text,TextInput,} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.logoContainer}>
-        <Image
-          style={styles.stretch}
-          source={require("./assets/palmtalk.png")}
-        />
+      <Image
+        style={styles.stretch}
+        source={require('../Test/assets/palmtalk.png')}
+      />
       </View>
       <View>
-        <Text
-          style={{
-            fontSize: 24,
-            fontWeight: "bold",
-            paddingLeft: 50,
-            paddingTop: 50,
-          }}
-        >
-          Getting Started
-        </Text>
-        <Text
-          style={{
-            fontSize: 14,
-            fontWeight: "medium",
-            paddingLeft: 50,
-            paddingTop: 5,
-            color: "#171717",
-          }}
-        >
-          Create an account to continue!
-        </Text>
+        <Text style = {{fontSize:24,fontWeight:'bold',paddingLeft:50,paddingTop:50}}>Getting Started</Text>
+        <Text style = {{fontSize:14,fontWeight:'medium',paddingLeft:50,paddingTop:5,color:'#171717'}}>Create an account to continue!</Text>
       </View>
       <View style={styles.loginInputContainer}>
-        <Text style={{ color: "#8F92A1", fontSize: 12 }}>Email</Text>
-
+        
+        <Text style={{color:'#8F92A1',fontSize:12}}>Email</Text>
+      
         <View style={styles.sectionStyle}>
           <Image
-            source={require("./assets/mail.png")}
+            source={require('../Test/assets/mail.png')}
             style={styles.imageStyle}
           />
           <TextInput
-            style={{ flex: 1 }}
+            style={{flex: 1,}}
             placeholder="Enter Your Email Here"
             underlineColorAndroid="transparent"
           />
         </View>
 
-        <Text style={{ color: "#8F92A1", fontSize: 12, paddingTop: 20 }}>
-          Username
-        </Text>
-
+        
+       
+        <Text style={{color:'#8F92A1',fontSize:12,paddingTop:20}}>Username</Text>
+      
         <View style={styles.sectionStyle}>
           <Image
-            source={require("./assets/favicon.png")}
+            source={require('../Test/assets/favicon.png')}
             style={styles.imageStyle}
           />
           <TextInput
-            style={{ flex: 1 }}
+            style={{flex: 1,}}
             placeholder="Enter Your Username Here"
             underlineColorAndroid="transparent"
           />
         </View>
 
-        <Text style={{ color: "#8F92A1", fontSize: 12, paddingTop: 20 }}>
-          Password
-        </Text>
 
+        <Text style={{color:'#8F92A1',fontSize:12,paddingTop:20}}>Password</Text>
+      
         <View style={styles.sectionStyle}>
           <Image
-            source={require("./assets/favicon.png")}
+            source={require('../Test/assets/favicon.png')}
             style={styles.imageStyle}
           />
           <TextInput
-            style={{ flex: 1 }}
+            style={{flex: 1,}}
             placeholder="Enter Your Password Here"
             underlineColorAndroid="transparent"
           />
         </View>
 
-        <Text style={{ color: "#8F92A1", fontSize: 12, paddingTop: 20 }}>
-          Confirm Password
-        </Text>
-
+        <Text style={{color:'#8F92A1',fontSize:12,paddingTop:20}}>Confirm Password</Text>
+      
         <View style={styles.sectionStyle}>
           <Image
-            source={require("./assets/favicon.png")}
+            source={require('../Test/assets/favicon.png')}
             style={styles.imageStyle}
           />
           <TextInput
-            style={{ flex: 1 }}
+            style={{flex: 1,}}
             placeholder="Re-Enter Your Password Here"
             underlineColorAndroid="transparent"
           />
         </View>
+
+      
+      
+       
+       
+        
       </View>
-    </View>
+
+
+    
+  </View>
+    
+    
   );
 }
 
@@ -104,34 +95,36 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 100,
   },
-  logoContainer: {
-    alignItems: "center",
+  logoContainer:{
+    alignItems:'center',
   },
 
   stretch: {
     width: 140,
     height: 120,
-    resizeMode: "stretch",
+    resizeMode: 'stretch',
   },
-  loginInputContainer: {
-    paddingTop: 50,
-    paddingLeft: 50,
-    paddingRight: 50,
+  loginInputContainer:{
+    paddingTop:50,
+    paddingLeft:50,
+    paddingRight:50,
   },
   sectionStyle: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
     borderBottomWidth: 0.5,
     height: 40,
+    
+   
   },
   imageStyle: {
     padding: 10,
     margin: 5,
     height: 25,
     width: 25,
-    resizeMode: "stretch",
-    alignItems: "center",
+    resizeMode: 'stretch',
+    alignItems: 'center',
   },
 });
