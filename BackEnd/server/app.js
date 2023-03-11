a2f995d01dc3e654676354d6a36a74a7ec0a9096
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-03-11 12:40:05 +0530

    fetch data from db for the login

--- BackEnd/server/app.js
+++ BackEnd/server/app.js
@@ -7,10 +7,10 @@ const User = mongoose.model("user");
 
 app.use(bodyParser.json());
 //const pass = "kBIJfGkEpX4vtIwB";
-const mongURI =
+const mongoURI =
   "mongodb+srv://Ravihara:kBIJfGkEpX4vtIwB@cluster1.ivh2y5n.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(mongURI, {
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -19,27 +19,67 @@ mongoose.connection.on("connected", () => {
   console.log("connected successfully");
 });
 mongoose.connection.on("error", (err) => {
-  console.log("error occured");
+  console.log("error occurred");
 });
 
 app.post("/send-data", (req, res) => {
   const user = new User({
     email: req.body.email,
     username: req.body.username,
-    password: req.body.password,
     repassword: req.body.repassword,
   });
   user
     .save()
     .then((data) => {
       console.log(data);
-      res.send("success data added");
+      res.send("successfully data added");
     })
     .catch((err) => {
       console.log(err);
     });
 });
 //login stuff
+app.post("/login", (req, res) => {
+  let { email = "", password = "" } = req.body;
+  email = email.trim();
+  password = password.trim();
+
+  if (email === "" || password === "") {
+    res.json({
+      status: "FAILED",
+      message: "Empty credentials supplied",
+    });
+  } else {
+    User.find({ email })
+      .then((data) => {
+        if (data.length) {
+          if (data[0].repassword === password) {
+            res.json({
+              status: "SUCCESS",
+              message: "Sign in successfully",
+              data: data,
+            });
+          } else {
+            res.json({
+              status: "FAILED",
+              message: "An error occurred while comparing passwords",
+            });
+          }
+        } else {
+          res.json({
+            status: "FAILED",
+            message: "Invalid credentials entered",
+          });
+        }
+      })
+      .catch((err) => {
+        res.json({
+          status: "FAILED",
+          message: "An error occurred while checking for an existing user",
+        });
+      });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("welcome to nodejs");
