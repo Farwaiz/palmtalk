668649f52909a387aa2756ffb13cceb151b56804
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-03-25 12:09:15 +0530

    changed some alignments

--- FrontEnd/Settings.js
+++ FrontEnd/Settings.js
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
     color: "orange",
     fontWeight: "bold",
     fontSize: 40,
-    marginTop: 170,
+    marginTop: 100,
     marginLeft: 92,
   },
   buttonArea: {
